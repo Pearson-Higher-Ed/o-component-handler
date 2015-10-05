b4a238f5cba2b91aad26e05fa94ce0de9a6225ec
Adam Armour <adam.armour@gmail.com>
2015-10-05 13:43:53 -0600

    chore: remove trailing comma

--- karma.conf.js
+++ karma.conf.js
@@ -107,7 +107,7 @@ module.exports = function (config) {
 
 		// Continuous Integration mode
 		// if true, Karma captures browsers, runs the tests and exits
-		singleRun: true,
+		singleRun: true
 
 	});
 };
